7e71858dc0b4951017865d3001f96f62d2c2d555
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2026-01-22 15:21:01 +0800

    Started working on module 3

--- src/pages/Module 1.py
+++ src/pages/Module 1.py
@@ -7,9 +7,7 @@ import streamlit as st
 st.title("Let's learn Trade!")
 
 
-st.markdown("This app is designed to help youths are very new to trading to get them started on these good financial habits.")
-st.markdown("This package is specifically designed for those who have very little knowledge of trading")
-st.markdown("This package entails 5 articles starting with foundation and ending with Quantitative Trading. After each article, you will have to complete a quiz in order to progress to the next article.")
+
 
 
 st.divider()
---
+++ src/pages/Module 3.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title("Let's learn Trade!")
+
+st.divider()
+
+st.header("Article 3: Indicator-Based Trading")
+
+
+st.subheader("Indicators are not magic. Past performance does not guarantee future results.")
+st.subheader("Indicators vs Reality: What do they actually do?")
+st.write("Indicators apply math to historical market data in order for traders to understand what is going on.")
+st.markdown("""
+Indicators are based on **past data**, which means:
+- They **summarize historical prices**.
+- They can indicate **volatility or momentum**.
+- They are inherently **lagging**, not predictive.
+""")
+st.write("This means that indicators should be used as tool, and not a prophecy")
+
+
